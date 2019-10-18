cdec0b140ed38e822489f047cdf5bc0e170449ea
Матвей <mrubinchik@mail.ru>
2019-10-18 17:06:22 +0300

    4 lesson

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -55,7 +55,7 @@ class Tests {
     }
 
     @Test
-    @Tag("Impossible")
+    @Tag("Trivial")
     fun lengthInMeters() {
         assertEquals(18.98, lengthInMeters(8, 2, 11), 1e-2)
         assertEquals(2.13, lengthInMeters(1, 0, 0), 1e-2)
